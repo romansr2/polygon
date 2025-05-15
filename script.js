d73a3757cffe947fdc9635cc5ea38c0cf8cbc41b
romansr2 <rskira2022@itcollege.lviv.ua>
2025-05-15 03:11:24 +0300

    v1

---
+++ script.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('navLinks');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('open');
+  navLinks.classList.toggle('active');
+});	
